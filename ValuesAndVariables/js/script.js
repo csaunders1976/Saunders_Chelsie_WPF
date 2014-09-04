33824ac3512dd66cf9da44747edf5adb0681118b
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-03 21:06:35 -0600

    Expressions
    Set up expressions assignment and followed expressions screencast

--- ValuesAndVariables/js/script.js
+++ ValuesAndVariables/js/script.js
@@ -1,11 +1,8 @@
-// JavaScript
+// Arrays
 
-var age; // Number variable
+var muppetNames = ["Kermit", "Miss Piggy"];
 
-var isStudent = false; //Boolean Variable
+muppetNames[1] = "Rizzo"; //lets me see what is in the index [1]
+console.log(muppetNames[1]); //change index #1 to Rizzo
 
-var firstName = "Kermit";
-
-var phase = 'I don\'t know \n You\'ll have to talk to the professor';
-
-
+var num = 2;
