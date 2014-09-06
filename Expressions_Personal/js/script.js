0509081801418f4d4505c82e534438a7a667f505
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-06 11:02:31 -0600

    Analyzed personal expression
    I wrote out everything I need for this expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,3 +4,14 @@ September 5 ,2014
 Expressions Personal
  */
 //How many boxes of flooring??
+
+// I want to know how much flooring I need to buy in order to have new floors.
+
+//Givens
+//Width of the room
+var width = prompt("What is the width of the room (in feet)?");
+//length of the room
+//square footage covered in each box
+//varible for result
+//What needs to be printed out in the console and the alert
+//It will require X amount of boxes to cover X amount of floor.
