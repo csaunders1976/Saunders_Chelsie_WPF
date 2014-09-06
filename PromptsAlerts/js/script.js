a0a72e41030edc160f06ef7db2359283994e4a36
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-06 10:24:28 -0600

    PromptsAndAlerts
    Followed Prompts and Alerts screecast

--- PromptsAlerts/js/script.js
+++ PromptsAlerts/js/script.js
@@ -3,4 +3,9 @@ Chelsie Saunders
 September 5 ,2014
 Prompts and Alerts
  */
-prompt();
+var width = prompt("Enter width");
+var height = prompt("Enter height");
+var area = width * height;
+//console.log(area);
+
+alert("The area of your rectangle is " + area);
