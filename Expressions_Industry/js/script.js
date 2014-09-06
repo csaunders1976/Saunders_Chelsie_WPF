7daa100308d29f2f9a744ce3439dd2f73fb39079
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-06 15:57:28 -0600

    Changed my Idea
    I changed my plan for the Professional expression. I decided to log my
    hours at work. It is more useful and I an cover the requirements for
    having an array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,4 +4,7 @@ September 5 ,2014
  Expressions Industry
  */
 
-//Digital Units converter
+//How much am I working?
+/* I often forget to record the hours that I work in freelance.
+This program is made to help me input this information with ease and average the amount I work.
+ */
