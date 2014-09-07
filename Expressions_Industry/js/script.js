1e66dbbbc4ec9745116494128afcf2420f2771cd
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-06 19:21:44 -0600

    Created prompts
    I was able to create all the needed prompts and test them in the console

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -16,4 +16,12 @@ var workHours = [];
 //I will have to all of the data that is entered into the array added up and averaged.
 //I will have to create an alert that gives information of the total hours and the average work day.
 workHours[0] = prompt("How many hours did you work on Monday?");
+workHours[1] = prompt("How many hours did you work on Tuesday?");
+workHours[2] = prompt("How many hours did you work on Wednesday?");
+workHours[3] = prompt("How many hours did you work on Thursday?");
+workHours[4] = prompt("How many hours did you work on Friday?");
 console.log(workHours[0]);
+console.log(workHours[1]);
+console.log(workHours[2]);
+console.log(workHours[3]);
+console.log(workHours[4]);
