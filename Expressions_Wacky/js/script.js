10bdf47cffb2a27a6394bb3ee32c8e34c90a2620
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-07 16:56:07 -0600

    scratch this
    i gave up. I will have to come up with a different project

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,21 +3,5 @@ Chelsie Saunders
 September 7 ,2014
 Expressions Wacky
  */
-//Digital Cootie Catcher
-
-/* First set of variables 3, 4, 5, 6
-Second set of variables yellow, black, orange, red
-third set of variables pink, purple, green, blue
- */
-
-var first = prompt("Ask a yes or no question. \n Enter a number, 3, 4, 5 or 6  ");
-console.log(first);
-
-var pink = 4;
-var green = 5;
-var red = 3;
-var yellow = 6;
-
-var second = prompt("Enter a color: " + pink +"," + yellow + "," + green  + ", or" + red +".");
-console.log(second);
-
+//scratch this
+var lost = 0;
