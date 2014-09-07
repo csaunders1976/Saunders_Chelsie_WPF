c3f6f8300c125f91a10c45ea510b74d034adbad8
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-07 16:18:09 -0600

    I spelled prompt wrong
    I was able to find the problem and correct it

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,5 +10,5 @@ Second set of variables yellow, black, orange, red
 third set of variables pink, purple, green, blue
  */
 
-var first = promt("Ask a yes or no question. \n Enter a number, 3, 4, 5 or 6  ");
-console.log(prompt);
+var first = prompt("Ask a yes or no question. \n Enter a number, 3, 4, 5 or 6  ");
+console.log(first);
