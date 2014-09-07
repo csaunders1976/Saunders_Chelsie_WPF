4fe4166f1468c336305dbc0291acc4049e4887ae
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-06 18:47:36 -0600

    Developing an understanding of how I am
    I am going through the process of how I am going to create this
    expression. I am a little stuck

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,3 +8,6 @@ September 5 ,2014
 /* I often forget to record the hours that I work in freelance.
 This program is made to help me input this information with ease and average the amount I work.
  */
+//Givens
+//I need to create an array that will hold the hours worked each day
+//I need to create a prompt for each day of the working week. I will need it to enter data for each index of the array
