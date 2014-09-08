e055a797af539f0948067c56dc0402aa522b1063
csaunders1976 <chelsiesaunders@mcwksnquiring.smcc.local>
2014-09-08 09:24:30 -0600

    tested console log for planet
    I did not get what I wanted. I was hoping for the value but I got the
    name of the planet instead

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -21,6 +21,8 @@ var pluto =  0.06;
 var neptune = 1.12;
 var moon = 0.16;
 //I will have to create a prompt to collect the potential planet
-var planet = prompt("Which heavenly body do you want to find out what you weigh on? \n Enter "+mercury+ "," + venus +","+ mars+","+ jupiter+","+ saturn+","+ uranus+ ","+ neptune+","+ pluto +","+ "or"+ moon+"? \n please do not capitalize the planets");
+var planet = prompt("Which heavenly body do you want to find out what you weigh on? \n Enter mercury, venus , mars, jupiter, saturn+, uranus, neptune, pluto, or moon? \n please do not capitalize the planets");
+//I am going to create a log in the console to see if I am getting the correct data out of my planet prompt
+console.log(planet);
 //I will have to create the result variable that will be printed out on the console and the final alert
 var result = weight*planet;
