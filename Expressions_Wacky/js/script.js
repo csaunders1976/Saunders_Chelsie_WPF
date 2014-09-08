3664a41c17ac131af96eaf283cbc7c044ccca5b5
csaunders1976 <chelsiesaunders@mcwksnquiring.smcc.local>
2014-09-08 10:13:12 -0600

    Successful
    I had a working prompt. I just need to turn the result into an alert
    instead of a prompt so it makes sense.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -20,10 +20,7 @@ var uranus = 0.89;
 var pluto =  0.06;
 var neptune = 1.12;
 var moon = 0.16;
-//I will have to create a prompt to collect the potential planet
-//var planet = prompt("Which heavenly body do you want to find out what you weigh on? \n Enter mercury , venus , mars , jupiter , saturn , uranus , neptune , pluto , or moon ? \n please do not capitalize the planets");
-//I am going to create a log in the console to see if I am getting the correct data out of my planet prompt
-console.log(planet);
-//I will have to create the result variable that will be printed out on the console and the final alert
-var result = weight*planet;
-console.log(result);
+
+//I will have to create a result prompt for each planet.
+
+prompt("You weigh " +mercury*weight +"lbs on Mercury");
