8984d83d2471875bafaa667532523984d4a490b0
csaunders1976 <chelsiesaunders@mcwksnquiring.smcc.local>
2014-09-08 10:07:32 -0600

    Everything is wrong
    I am going to have to attack this on a different angle. I am not able
    to display the variable by its name in the prompt and have it work
    properly in an equation

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,7 +2,7 @@
 Chelsie Saunders
 September 7 ,2014
 Expressions Wacky
- */
+*/
 //How much do I weigh on the moon?
 //This is my third try. I don't know how wacky it is but I think it is doable.
 
@@ -10,7 +10,7 @@ Expressions Wacky
 //I will have to create a prompt that requests your weight
 var weight = prompt("How much do you weigh in pounds?");
 console.log(weight);
-//I will have to create variable that contain the equations for each planet. This will require the a number that compares the gravitational pull of each planet compared to Earth.
+
 var mercury = 0.38;
 var venus = 0.91;
 var mars = 0.38;
@@ -21,8 +21,9 @@ var pluto =  0.06;
 var neptune = 1.12;
 var moon = 0.16;
 //I will have to create a prompt to collect the potential planet
-var planet = prompt("Which heavenly body do you want to find out what you weigh on? \n Enter mercury, venus , mars, jupiter, saturn+, uranus, neptune, pluto, or moon? \n please do not capitalize the planets");
+//var planet = prompt("Which heavenly body do you want to find out what you weigh on? \n Enter mercury , venus , mars , jupiter , saturn , uranus , neptune , pluto , or moon ? \n please do not capitalize the planets");
 //I am going to create a log in the console to see if I am getting the correct data out of my planet prompt
 console.log(planet);
 //I will have to create the result variable that will be printed out on the console and the final alert
 var result = weight*planet;
+console.log(result);
