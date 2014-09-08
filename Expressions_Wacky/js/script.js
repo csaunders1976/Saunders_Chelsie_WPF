80a3ce496f68834fb1d217ef816354f97944f465
csaunders1976 <chelsiesaunders@mcwksnquiring.smcc.local>
2014-09-08 10:58:53 -0600

    Console logs for each planet

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -26,16 +26,25 @@ var neptune = 1.12;
 var moon = 0.16;
 
 var mass = weight/earth;
-console.log("This is your mass =" +mass".");
+console.log("This is your mass =" +mass+".");
 
 //I will have to create a result alert for each planet.
 
 alert("You weigh " +mercury*mass +"lbs on Mercury");
+console.log("This is your weight on Mercury = " + mercury*mass + ".")
 alert("You weigh " +venus*mass +"lbs on Venus");
+console.log("This is your weight on Venus = " + venus*mass + ".")
 alert("You weigh " +mars*mass +"lbs on Mars");
+console.log("This is your weight on Mars = " + mars*mass + ".")
 alert("You weigh " +jupiter*mass +"lbs on Jupiter");
+console.log("This is your weight on Jupiter = " + jupiter*mass + ".")
 alert("You weigh " +saturn*mass +"lbs on Saturn");
+console.log("This is your weight on Saturn = " + saturn*mass + ".")
 alert("You weigh " +uranus*mass +"lbs on Uranus");
-alert("You weigh " +pluto*mass +"lbs on Pluto");
+console.log("This is your weight on Uranus =" + uranus*mass + ".")
+alert("You weigh " +pluto*mass +"lbs on  Pluto");
+console.log("This is your weight on Pluto = " + pluto*mass + ".")
 alert("You weigh " +neptune*mass +"lbs on Neptune");
+console.log("This is your weight on Neptune = " + neptune*mass + ".")
 alert("You weigh " +moon*mass +"lbs on Moon");
+console.log("This is your weight on Moon = " + moon*mass + ".")
