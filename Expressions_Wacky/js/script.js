4c20f4790db6283266ed068dd80a4b29f3f407ef
csaunders1976 <chelsiesaunders@mcwksnquiring.smcc.local>
2014-09-08 09:11:12 -0600

    created prompt for planets
    created prompt for planets

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -20,5 +20,7 @@ var uranus = 0.89;
 var pluto =  0.06;
 var neptune = 1.12;
 var moon = 0.16;
+//I will have to create a prompt to collect the potential planet
+var planet = prompt("Which hevenly body do you want to find out what you weigh on? \n Enter mercury, venus, mars jupiter, saturn, uranus, neptune, pluto, or moon? \n please do not capitalize the planets");
 //I will have to create the result variable that will be printed out on the console and the final alert
 var result = weight*X;
