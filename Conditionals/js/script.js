c6f89b6c4ff1b1d3587059c880ad3e7cc5890ef7
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-08 20:24:52 -0600

    If and else
    followed screen-cast

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,15 +1,17 @@
 /*
 Chelsie Saunders
 September 8 ,2014
-Condition with Expressions
+if and else
  */
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
    // code performed if condition is true
    console.log("You can ride the coaster!");
+}else{
+    // if the condition is false
+    console.log("Sorry kid, you have some growing to do!");
 }
