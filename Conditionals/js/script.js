467ce66513b4735009d5c2eb46a1582d2b830f0c
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-10 19:44:53 -0600

    Set up week 2 files
    created html and js files on web storm

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,17 +1,20 @@
 /*
 Chelsie Saunders
-September 8 ,2014
-if and else
+September 10 ,2014
+Conditional Logic = else if
  */
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
    // code performed if condition is true
    console.log("You can ride the coaster!");
+}else if (kidHeight > wParentHeight){
+    // you can ride with a parent present
+    console.log("You can ride with a parent.")
 }else{
-    // if the condition is false
     console.log("Sorry kid, you have some growing to do!");
 }
---
+++ GoToTrainingWeek2/js/week.js
@@ -0,0 +1,7 @@
+/*
+Chelsie Saunders
+September 10, 2014
+WPF Section 01
+Go To Training Week #2
+ */
+alert("Testing 1, 2, 3!")
