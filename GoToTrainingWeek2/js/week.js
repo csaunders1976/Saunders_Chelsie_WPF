6df7eee35b62e3ae2904c04c8b4c5cfbf179f89b
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-10 19:55:28 -0600

    created prompt for users name
    created prompt for users name

--- GoToTrainingWeek2/js/week.js
+++ GoToTrainingWeek2/js/week.js
@@ -5,3 +5,14 @@ WPF Section 01
 Go To Training Week #2
  */
 alert("Testing 1, 2, 3!")
+
+//Create a calculator for age
+
+//Ask the user their name
+var name = prompt("Please type in your name:");
+
+//test to make sure this works
+console.log(name);
+
+//Alert to the user and explain what we are doing
+alert("Welcome "+name+" Lets figure out how are old you are.")
