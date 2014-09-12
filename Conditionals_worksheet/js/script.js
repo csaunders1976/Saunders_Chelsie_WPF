292425c0e9cff9293bf12e19a1f41b4a8a2497ad
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-12 15:10:06 -0600

    created variables and alerts for Tire Pressure
    created variables and alerts for Tire Pressure

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -155,13 +155,15 @@ To meet to maintenance standards a car’s front two tires should have the
  same pressure and the back two tires should have the same pressure.
  But the front tires and the rear tires can have different pressure than each other,
  so it is not necessary for all four tires’ pressure to be the same.
- Create a single conditional that would determine if the tires of a given car are up to spec.
+ Create a single conditional that would determine if the tires of a given car are up to spec.*/
 
 
-    Given:
-Pressure for each tire of the car (in psi) in an array.
-    Result To Print Out:
-    “The tires pass spec!” Or  “ Get your tires checked out!”
+//    Given:
+//Pressure for each tire of the car (in psi) in an array.
+var pressure = [];
+//    Result To Print Out:
+alert("The tires pass spec!"); 
+alert("Get your tires checked out!");
 
 /***Movie Ticket Price
 
