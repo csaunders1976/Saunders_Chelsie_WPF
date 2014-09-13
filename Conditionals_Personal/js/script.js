3c746ebabb59059aecb9c636129b4c9343ef0001
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-12 21:08:01 -0600

    first set of variables and first condition
    first set of variables and first condition

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,34 @@ Chelsie Saunders
 September 12 ,2014
 Conditionals Personal
  */
+//Cootie Catcher
+
+// First set of variables 3, 4, 5, 6
+var first = prompt("Pick (enter) a number: 3, 4, 5 or 6");
+
+// Second set of variables red, orange, yellow, black
+var second = prompt("Pick (enter) a color: red, orange, yellow or black");
+
+var third;
+
+// forth  Set of variables purple, green, blue, pink
+var fourth = prompt("Pick (enter) another color: purple, green, blue, pink");
+
+if(second = "red"){
+    third = first*1;
+}else if(second = "orange"){
+    third = first*2;
+}else if(second = "yellow") {
+    third = first * 3;
+}else if(second = "black"){
+    third = first * 4;
+}
+
+alert("No");
+alert("Yes");
+alert("Maybe");
+alert("Not Sure");
+alert("Answer is cloudy. Ask again Later");
+alert("Definitely");
+alert("Unlikely");
+alert("Perhaps");
