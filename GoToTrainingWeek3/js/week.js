4dae86c131618eed74bfa4658230b3e62d69f6c7
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-19 12:12:37 -0600

    Counted # of pears using 'if statements
    Counted # of pears using 'if statements

--- GoToTrainingWeek3/js/week.js
+++ GoToTrainingWeek3/js/week.js
@@ -23,3 +23,42 @@ console.log(bowlOfFruit.length);
 
 //Now can I get the text of "banana"?
 console.log(bowlOfFruit[1]);
+
+//Test each fruit to see if it is a pair
+//If it is, lets add it to a total# of pears
+// create a variable to hole the # of pears
+var totalNumPears = 0;
+
+//if statement - conditional
+
+if(bowlOfFruit[0] === 'pear'){
+  //this code will run if the condition is true
+  //Then add 1 to our total number of pears
+    totalNumPears++;
+}
+
+//Test all of the fruits in the bowl
+if(bowlOfFruit[1] === 'pear'){
+    //this code will run if the condition is true
+    //Then add 1 to our total number of pears
+    totalNumPears++;
+}
+
+if(bowlOfFruit[2] === 'pear'){
+    //this code will run if the condition is true
+    //Then add 1 to our total number of pears
+    totalNumPears++;
+}
+
+if(bowlOfFruit[3] === 'pear'){
+    //this code will run if the condition is true
+    //Then add 1 to our total number of pears
+    totalNumPears++;
+}
+
+if(bowlOfFruit[4] === 'pear'){
+    //this code will run if the condition is true
+    //Then add 1 to our total number of pears
+    totalNumPears++;
+}
+console.log(totalNumPears+ " pears in our bowl");
