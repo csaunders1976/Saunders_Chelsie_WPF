173a3050edd4fea24da09dfaf90c7f317abc62b5
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-19 11:54:40 -0600

    set up bowl of fruit array
    covered basics of array

--- GoToTrainingWeek3/js/week.js
+++ GoToTrainingWeek3/js/week.js
@@ -5,12 +5,21 @@ WPF Section 01
 Go To Training Week #3
  */
 
-alert("Testing...can you hear me?");
-var wantDog = prompt("Do you want a dog? Yes or No?");
+//alert("Testing...can you hear me?");
 
-if(wantDog === "Yes"){
-    //The prompt more questions here
-    var breed = prompt("What breed do you want?")
-}else{
-    console.log("That's sad, dogs are adorable!");
-}
+// Code a check to see how many pears are in a fruit bowl.
+//Array - hold multiple variables under one name - variables of variables
+
+var bowlOfFruit = ["apple", "banana", "pear", "peach", "pear"];
+
+console.log(bowlOfFruit);
+
+//How long is this array?
+//Array Length
+//Dot Syntax - "use a period"
+
+
+console.log(bowlOfFruit.length);
+
+//Now can I get the text of "banana"?
+console.log(bowlOfFruit[1]);
