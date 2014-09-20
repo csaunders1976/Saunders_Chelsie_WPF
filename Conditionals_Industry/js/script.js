1e20303ed94c1ef99b90f1d37dcfef3a6ae5daee
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-20 14:19:56 -0600

    created conditional statement
    I created a statement that gives the appropriate response depending on
    the total hours worked this week.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -35,3 +35,16 @@ for(var i=0; i<workHours.length; i++){
 }
 
 console.log(totalHours);
+
+//I have to create a conditional statement that tells me if I should keep working for this guy
+
+if(totalHours >= 10){
+    alert("You need to tell this guy he needs to pay you before you do any more work!");
+    console.log("You need to tell this guy he needs to pay you before you do any more work!");
+}else if(totalHours >=20){
+    alert("You need to tell this guy your done working for him!");
+    console.log("You need to tell this guy your done working for him!");
+}else{
+    alert("Be patient, you will have to be patient.");
+    console.log("Be patient, you will have to be patient.");
+}
