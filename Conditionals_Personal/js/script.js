0080d1475d9ace44f884dcf9f50dbe6fe9a239f0
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-20 13:12:00 -0600

    Created first prompt
    I created the prompt to ask what time I finished my homework

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,18 +8,21 @@ Conditionals Personal
 
 //First I need to remind myself of the structure of a Ternary (condition) ? do if true : do if false
 
-// I would like to finish my homework. It has been a really heartbreaking week and I need some help with my focus. I will reward
-// myself if I complete my homework.
+/* I would like to finish my homework. It has been a really heartbreaking week and I need some help with my focus.
+I will reward myself if I complete my homework. Normal getting my homework done is award enough but with the death of a
+close friend and co-worker I have had difficulty concentrating.
+ */
 
 //I will create my statement in English
 // (If get my homework done before 5pm) ? I will take my husband out for dinner : I will not be able to go out to dinner;
 
-// I will create my givin:
+// I will create my givin's:
 
-//I will have to create a prompt in order to ask for the time of completion
-var time;
 //I will have to create a boolean for if my homework is completed
 var homework;
+
+//I will have to create a prompt in order to ask for the time of completion
+var time = prompt("What time did you finish your homework? (Military time in this format '1300' please.)");
 
 //this is my attempt at the ternary. I referred to line 9 for structure and line 15 for content.
 (homework === "true" && time <= 1700) ? alert("Good job. Take your man out for dinner!") : alert("It looks like you get ramen noodles tonight.");
