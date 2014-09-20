4020cd7e22594f14ec2d14f2d1497522f833ffd5
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-20 12:40:27 -0600

    Created variables
    I created a couple of variables to start my assignments

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,18 @@ Chelsie Saunders
 September 12 ,2014
 Conditionals Personal
  */
+
+//This is where I am going to do my Ternary Operator for the requirements of this assignment
+
+//First I need to remind myself of the structure of a Ternary (condition) ? do if true : do if false
+
+// I would like to finish my homework. It has been a really rough week and I need some help with my focus. I will reward
+// myself if I complete my homework.
+
+//I will create my statement in English
+// (If get my homework done before 5pm) ? I will take my husband out for dinner : I will not be able to go out to dinner;
+
+// I will create my givins:
+
+var time;
+var homework;
