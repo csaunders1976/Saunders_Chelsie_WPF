bcd9f4c9d508098e1357bb28576e3aa5dabb0035
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-21 13:54:43 -0600

    Variable Scope
    Variable Scope

--- FunctionsScreenCast/js/script.js
+++ FunctionsScreenCast/js/script.js
@@ -3,23 +3,21 @@ Chelsie Saunders
 September 21 ,2014
 Functions Screen-cast
  */
-//Function Invocation
+//Variable Scope
 /*
 Basic structure of a function
 function functionName(){
     //code the function runs
 }
     */
-
-function outputMsg(){
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+   // console.log(area);
 }
+console.log(width);
 //calling the function
 calcArea();
