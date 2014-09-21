b7d3294e155411111778f07c6547b48bb6dec16a
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-21 16:49:42 -0600

    created the formula for how many stings.
    created the formula for how many stings.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -15,7 +15,7 @@ var radius = 10;
 
 // --- function for calculating circumference ---
 function calcCir(r){
-    // formula Circumference of the circle
+    // formula for circumference of the circle
     var cir = r * 2 * Math.PI;// Yeah, I am a bad girl. I read ahead again. I had to add the Math.PI so things are neat and tidy.
 // Return: I need to return this value so I can calculate my result
     return cir;
@@ -31,11 +31,17 @@ console.log("The circumference of the circle is " + result.toFixed(3));
 
 // It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
 
-// Given:
-// Victim’s weight (in pounds)
-// Parameter(s) for function:
+
 // Victim’s weight (in pounds)
 var weight = 182;
+
+// --- function for calculating how many stings from a bee does it take to kill an animal ---
+function killerBee(w){
+    //formula for calculating how many bee stings to kill
+    var death = w/8.666666667;
+// Return: I need to return this value so I can get a result outside of this function
+}
+
 
 // Return:
 // Number of Bee stings
