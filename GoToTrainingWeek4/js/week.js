5df8650b2ec1ff1610f58b53fd78dc65ede49313
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-24 15:30:07 -0600

    we have both user inputs

--- GoToTrainingWeek4/js/week.js
+++ GoToTrainingWeek4/js/week.js
@@ -12,10 +12,29 @@ Go To Training Week #4
 var min = prompt("Let's get a random number between 2 numbers. \nPlease type in a minimum number. ");
 
 //Validate the user prompt
-while(min===""){
+while(min==="" || isNaN){
     //reprompt the user
     min= prompt("Please enter a number... or this won't work!")
 
 }
 //Test to make sure min is a number
 console.log(min);
+
+//console.log(isNaN(7));// false if it is a number
+//console.log(isNaN("monkey"));//text gives us a true
+
+while(isNaN(min)){
+    min= prompt("Please enter a number... or this won't work!")
+}
+
+//Ask the user for a max number
+
+var max = prompt("Please type in max number");
+
+//Validate the user prompt
+//best way (so far) to validate - combine the while loops
+
+while (max === "" || isNaN(max)){
+    //reprompt the use
+    max = prompt("Please type in max number.");
+}
