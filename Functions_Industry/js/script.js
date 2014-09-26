6d2bfaef25eecf8290107e94e2958314db78a5d7
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-25 20:32:10 -0600

    created an alert explaining what information
    created an alert explaining what information we are trying to get. I
    also created the prompt with while loop validation

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,8 +8,12 @@ Functions Industry
 // a point is 1/72 of an inch
 // a pixel is 1/96 of an inch
 
-Givens
-var points;
+//Givens
+alert("We are going to calculate how many pixels your font measures");
+var points = prompt("How many points is your font?");
+while(point ==="" || isNaN(points)){
+    prompt("How many points is your font? Numbers only please");
+}
 var results;
 
 var convert = function(){
