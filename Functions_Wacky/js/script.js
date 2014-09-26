fbb47d842206c05c2bbcdc34c50e2af14145f556
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-25 19:30:04 -0600

    created my variable that calculates mortality
    So sad. Many of the tribbles have died from eating poison grain!

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -22,16 +22,27 @@ easier to calculate.R.I.P Gene Roddenberry*/
 /*Givens
  How many hours lapsed
  how many starting Tribbles*/
-var tribbles = 3;
-var time = 24;
+var tribbles = 1;
+var time = 72;
 
 var fertility = function (trib,hours){
     //calculation to find how many tribbles reproduce over times
 //    var crazyTribbles = (Math.round(trib + trib * Math.random()*(10)) )*(hours/12);
 // Now that I have tested this I got a result of 64. I want to see if my math is right so I will test it without the Math.random
-    var crazyTribbles = (Math.round(trib + trib * 10) )*(hours/12);
+// Now that I have tested my math I have figured out that my math is terribly wrong.
+// My numbers are not anywhere near Mr. Spock's.
+// I am going to have to use exponents.
+
+
+//The JavaScipt for exponents is Math.pow(base, exponent);
+    //var crazyTribbles = (Math.round(trib + trib * 10)); ********Now I want to apply this with a mortality rate...
+    // I must consider that some of the tribbles have consumed some of the tainted grain and have died. (Sorry if you are not a Treky)
+
+    var crazyTribbles = (Math.round(trib + trib * Math.random()*(10)) );
+//Hooray! This is the correct calculation. Spock would be proud!
+    var furEverywhere = Math.pow(crazyTribbles, hours/12);
     //return value
-    return crazyTribbles;
+    return furEverywhere;
 }
 // this is the variable to catch the number from the  fertility function
 var result = fertility(tribbles, time);
