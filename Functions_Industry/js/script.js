6379ede30061d1aaddaf145312b572400940a886
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-25 20:43:54 -0600

    created function
    created function and console log to test

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,17 +5,31 @@ Functions Industry
  */
 /// Typography Calculator
 
+// I don't really know the exact details about what make a point, a pixal or an EM. I decided to compare
+//of them to gain a better understanding
 // a point is 1/72 of an inch
 // a pixel is 1/96 of an inch
 
+/////_____this is just me thinking my calculation through
+//points = pixels * 72 / 96
+//points * 96 = pixels * 72
+// (points * 96)/72 = pixels
+// now it is time to write it in code
+
 //Givens
+
+//This alert just explains what we are doing
 alert("We are going to calculate how many pixels your font measures");
 var points = prompt("How many points is your font?");
-while(point ==="" || isNaN(points)){
+while(points ==="" || isNaN(points)){
     prompt("How many points is your font? Numbers only please");
 }
-var results;
 
-var convert = function(){
 
+var convert = function(p){
+    var num = p * 96 / 72;
+    return num
 }
+
+var result = convert(points);
+console.log(result);
