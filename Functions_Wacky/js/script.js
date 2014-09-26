8e75cbe511d149df717ac4ac77b96ae310dffc20
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-25 18:41:03 -0600

    created given variables
    created given variables

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,12 +6,25 @@ Functions Wacky
 //Naughty Tribbles
 
 /* I originally had breeding bunnies. I figured that I had to consider that a large portion
-of rabbits were born male. I was not sure I could calculate that. Fortuatly I am a Star Trek Fan
+of rabbits were born male. I was not sure I could calculate that. Fortunatly I am a Star Trek Fan
 and remembered that Tribbles are asexual and born pregnant. That would certainly make things
-easier to calulate.R.I.P Gene Roddenberry*/
+easier to calculate.R.I.P Gene Roddenberry*/
 
 
 //Givens
 // How many hours lapsed
 // how many starting Tribbles
 
+// Tribbles can produce up to 10 offspring every 12 hours.
+// I will have to multiply the starting amount of tribbles by 10 plus the original amount of tribbles.
+// Rather than multiplying by ten I am going to use Math.round 1 - ten to consider tribble mortality
+// Then I will have to multiply that number every 12 hours.
+
+
+
+var tribbles;
+var time;
+
+function fertility(){
+
+}
