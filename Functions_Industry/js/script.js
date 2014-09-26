e20758180e544bd82dd1eabd96f83e22cf2fd3ee
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-09-25 20:27:43 -0600

    created variables for givens
    created variables for givens

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,5 +3,15 @@ Chelsie Saunders
 September 21 ,2014
 Functions Industry
  */
-/// CSS Typography Calculator
+/// Typography Calculator
 
+// a point is 1/72 of an inch
+// a pixel is 1/96 of an inch
+
+Givens
+var points;
+var results;
+
+var convert = function(){
+
+}
